d5022b146b2a42e0f09c5b957f37c14de83491cf
cl19365 <3301936579@qq.com>
2022-03-16 22:03:07 +0800

    提交

---
+++ 记事本/js/homepage.js
@@ -0,0 +1,12 @@
+var f=new Vue({
+    el:"#f",
+    data:{
+        showLine:false
+    },
+    methods:{
+        showLine:function(){
+            this.showLine=!this.showLine;
+        }
+    }
+})
+
